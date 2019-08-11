65f65de3dfd0fff609fce36520b0815cf8a375c3
Christopher M. Dicely <cmdicely@gmail.com>
2019-08-11 02:18:35 -0700

    Update cli,py with copyright and header info

--- imperium/cli.py
+++ imperium/cli.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+Imperium's CLI
+"""
+
+__copyright__ = "Copyright 2019, Christopher M. Dicely"
+__license__ = "GPL-2.0-only"
+# This program is free software; you can redistribute it 
+# and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation; version 2.
+
 import click
 
 @click.group()
